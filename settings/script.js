267ac2c1034a4af3b4e14a2addb18d6269dd5c44
Lucas Meiner-Duedahl <lucasmeiner-duedahl@lucass-air-2.home.arpa>
2026-05-16 17:32:36 +0200

    update

--- settings/script.js
+++ settings/script.js
@@ -1,6 +1,9 @@
 let settingsContainer = document.getElementById('settings-container');
 let currentUrl = window.location.href.split('?')[0].replace('index.html', '').replace(/\/$/, '');
-settingsContainer.src = `https://nuttylmao.github.io/widget-customizer?settingsJson=${currentUrl}/settings.json`;
+settingsContainer.src = `https://nuttylmao.github.io/widget-customizer?settingsJson=https://raw.githubusercontent.com/LucasM-D/MultiChatOverlayEseninocafe/main/settings/settings.json`;
+
+let widget = document.getElementById("widget");
+widget.src = getParentUrl();
 
 function reloadWidget(data) {
     let widget = document.getElementById("widget");
